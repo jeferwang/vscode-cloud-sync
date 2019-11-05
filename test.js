40aade50b012dd3d074b6c1aa4104f12eb2c103d
jeferwang <1737723363@qq.com>
2019-11-05 16:33:40 +0800

    test

--- test.js
+++ test.js
@@ -3,19 +3,51 @@ const HttpsProxyAgent = require('https-proxy-agent')
 const Agent = require('http').Agent
 
 const github = new GithubApi({
-  auth: 'd657f6bd0dbfdfc6d5230ef0ccb59d5f06a814fd',
+  auth: 'token 170fb493e738a19367cd55737bdd74cc19f13064',
   request: {
     agent: new HttpsProxyAgent('http://127.0.0.1:12639/')
   }
 })
 
-github.gists
-  .get({
-    gist_id: '123'
-  })
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.error(err)
-  })
+// github.gists
+//   .update({
+//     gist_id: '1dd8db54602c8068f488d6a8b02720f1',
+//     files: {
+//       'hello.php': null
+//     },
+//     description: '测试222'
+//   })
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//   })
+
+// github.gists
+//   .create({
+//     files: {
+//       'demo.php': {
+//         content: `
+//         <?php
+//           echo "demo";
+//         ?>
+//         `
+//       },
+//       'hello.php': {
+//         content: `
+//         <?php
+//           echo "hello world";
+//         ?>
+//         `
+//       }
+//     },
+//     public: false,
+//     description: '测试'
+//   })
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//   })
